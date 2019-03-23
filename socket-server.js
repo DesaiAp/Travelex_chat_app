16e986a0db1320613e97cbae6f1d3372b4068358
Desai <desaiAp@emea.travelex.net>
2019-03-23 13:57:58 +0530

    first commit

--- socket-server.js
+++ socket-server.js
@@ -3,25 +3,25 @@ var app = express();
 var http = require("http");
 var server = http.createServer(app);
 var io = require("socket.io")(server);
-var port=process.env.PORT || 3000;
 
-io.on("connection", client =>{
-    console.log("Client Connected"+client.id);
-    client.emit("Acknowledge", {message:"You are connected"});
-    client.on("msgToServer", (chatterName,msg)=>{
-        console.log(chatterName +" says :" +msg);
-        client.broadcast.emit("ServerMsg",chatterName, msg)
-        client.emit("ServerMsg","Me", msg)
+io.on("connection", client => {
+    console.log("Client Connected");
+    client.emit("acknowledge", {message : "You are connected."})
+    client.on("msgToServer", (chatterName, msg) => {
+        console.log(chatterName + " says : " + msg);
+        client.broadcast.emit("serverMsg", chatterName, msg)
+        client.emit("serverMsg", 'Me' ,msg)
     })
-    client.on("disconnect",()=>{
 
+    client.on("disconnect", ()=>{
+        // Message to be saved in Mongo
     })
 })
 
-app.get("/",(req, res)=>{
+app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/public/socket-client.html")
 })
 
 server.listen(3000, ()=>{
-    console.log("Socket Server running on Port "+port);
+    console.log("Socket server running on Port 3000");
 })
